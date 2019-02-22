1aadbb3b84b521866944f372538dcc3e324b035b
Thorben Bender <bender.thorben0@gmail.com>
2019-02-22 12:59:51 +0100

    added prop-types

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react';+
+import PropTypes from 'prop-types';
 import Card from './Card';
 
 const Cards = props => {
@@ -10,6 +11,9 @@ const Cards = props => {
           new Card component for each passing the card as the only prop*/}
     </div>
   )
+}
+Cards.PropTypes = {
+  cards: PropTypes.arrayOf(PropTypes.object)
 }
 
 // Make sure you include prop types for all of your incoming props
