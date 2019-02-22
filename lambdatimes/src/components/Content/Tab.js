1f6e7c117eac53879338c305acf26dd4f108239e
Thorben Bender <bender.thorben0@gmail.com>
2019-02-22 14:38:06 +0100

    add styling to the tabs

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+let StyledTab = styled.div`
+  margin: 0% 2%;
+  border: 1px solid black;
+  padding: 1px;
+`
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
       
   return (
-    <div
-      className={''}
+    <StyledTab
       onClick={() => props.changeSelected(props.tab)}
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
       // }
     >
       {props.tab.toUpperCase()}
-    </div>
+    </StyledTab>
   );
 };
 
