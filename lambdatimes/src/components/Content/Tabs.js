90409e2b35038792aa3909419495b96ff68ce8c6
Thorben Bender <bender.thorben0@gmail.com>
2019-02-22 10:53:18 +0100

    done with rendering the data

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Tab from './Tab';
+import {tabData} from '../../data';
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
+        {
+          tabData.map((tab, idx) => <Tab tab={tab} changeSelected={props.changeSelected} selectedTab={props.selectedTab} key={idx}/>)
+          /* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
       </div>
     </div>
